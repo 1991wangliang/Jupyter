22edf8bf809112f1d3c0e35628a6abc31d728312
lorne <1991wangliang@gmail.com>
2022-07-07 18:00:00 +0800

    add run res

--- notebooks/005_neural-style-pytorch/d2l.py
+++ notebooks/005_neural-style-pytorch/d2l.py
@@ -77,8 +77,7 @@ class Animator:
 
 
 def try_gpu(i=0):
-    """如果存在，则返回gpu(i)，否则返回cpu()
-
+    """如果存在，则返回gpu(i)，否则返回cpu()        
     Defined in :numref:`sec_use_gpu`"""
     if torch.cuda.device_count() >= i + 1:
         return torch.device(f'cuda:{i}')
